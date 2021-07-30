fa24be3c099f6b48e4fd6822e9510d988504ac5d
Muhammad Maqsood <mmaqsood@accelaero.com>
2021-07-30 13:52:15 +0400

    project codebase created with andriod studio

---
+++ app/src/main/java/com/nyt/mostpopular/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nyt.mostpopular.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nyt.mostpopular.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
