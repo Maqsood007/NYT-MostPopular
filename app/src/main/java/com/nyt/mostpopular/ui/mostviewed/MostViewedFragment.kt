e1b4a49b2d1aef37e9db5e5de37d7cee60e3e3f0
Muhammad Maqsood <mmaqsood@accelaero.com>
2021-08-02 01:13:15 +0400

    README added for the details iformation

--- app/src/main/java/com/nyt/mostpopular/ui/mostviewed/MostViewedFragment.kt
+++ app/src/main/java/com/nyt/mostpopular/ui/mostviewed/MostViewedFragment.kt
@@ -1,5 +1,6 @@
 package com.nyt.mostpopular.ui.mostviewed
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu
@@ -63,6 +64,7 @@ class MostViewedFragment : BaseFragment() {
         addStateObserver()
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     private fun setUpAdapter() {
 
         MostViewedArticlesListAdapter().let {
